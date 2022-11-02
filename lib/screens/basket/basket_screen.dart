78ebf6d00ef3c04dda92f646add9468de02a8d10
metehangurgentepe <73113420+metehangurgentepe@users.noreply.github.com>
2022-11-03 00:37:10 +0300

    basket screen ve pay screen tasarımları değiştirildi. basket screen'de methodlar düzgün çalışmıyor bakılması lazım.

--- lib/screens/basket/basket_screen.dart
+++ lib/screens/basket/basket_screen.dart
@@ -3,26 +3,48 @@ import 'package:waitech/config/theme.dart';
 import 'package:waitech/config/app_router.dart';
 import 'package:waitech/screens/basket/pay_screen.dart';
 
-class BasketScreen extends StatelessWidget {
+class BasketScreen extends StatefulWidget {
   static const String routeName = '/basket';
+
+  const BasketScreen({super.key});
 
   static Route route() {
     return MaterialPageRoute(
-        builder: (_) => const BasketScreen(),
+        builder: (_) => BasketScreen(),
         settings: const RouteSettings(name: routeName));
   }
 
-  const BasketScreen({Key? key}) : super(key: key);
+  @override
+  State<BasketScreen> createState() => _BasketState();
+}
+
+class _BasketState extends State<BasketScreen>{
+  late var itemNumbers=1;
+  var itemRow=4;
+   addItems() => itemNumbers++;
+  removeItems() {
+    itemNumbers--;
+    if(itemNumbers==0){
+      itemRow--;
+    }
+    else if(itemNumbers<=0){
+      itemRow=0;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text('Sepet'),
-          actions: <Widget>[
-            IconButton(onPressed: (){}, icon: Icon(Icons.delete))
-          ],
-        ),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Sepet'),
+        actions: <Widget>[
+          IconButton(onPressed: (){
+            itemRow=0;
+          }, icon: Icon(Icons.delete))
+        ],
+      ),
       body: Padding(padding: const EdgeInsets.all(20.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -38,56 +60,58 @@ class BasketScreen extends StatelessWidget {
             SizedBox(height: 20),
             ListView.builder(
                 shrinkWrap: true,
-                itemCount: 4,
+                itemCount: itemRow,
                 itemBuilder: (context,index){
-              return Container(
-                width: double.infinity,
-                margin: const EdgeInsets.only(top:5),
-                padding:const EdgeInsets.symmetric(horizontal: 20,vertical: 30),
-                decoration: BoxDecoration(color:Colors.white,borderRadius: BorderRadius.circular(50.0)),
-                child: Row(children: [
-                    Text('1x',
-                      style: TextStyle(
-                        fontFamily: 'Monoton-Regular',
-                        fontSize: 20,
-                        color: Theme.of(context).primaryColor,
-                      ),
-                    ),
-                  SizedBox(width: 20),
-
-                  Expanded(
-                    child:
-                    Column(
-                      children: [
-                        Text('Margharita Pizza',
-                          style: TextStyle(
-                            fontFamily: 'Monoton-Regular',
-                            fontSize: 18,
-                          ),
+                  return Container(
+                    width: double.infinity,
+                    margin: const EdgeInsets.only(top:5),
+                    padding:const EdgeInsets.symmetric(horizontal: 20,vertical: 30),
+                    decoration: BoxDecoration(color:Colors.white,borderRadius: BorderRadius.circular(50.0)),
+                    child: Row(children: [
+                      Text('$itemNumbers x',
+                        style: TextStyle(
+                          fontFamily: 'Monoton-Regular',
+                          fontSize: 20,
+                          color: Theme.of(context).primaryColor,
                         ),
-                        Text('4.99\$',
-                          style: TextStyle(
-                            fontFamily: 'Monoton-Regular',
-                            fontSize: 18,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(width: 15),
-                  Row(
-                    children: [
-                      IconButton(onPressed: onPressed(), icon: Icon(Icons.remove, color:Theme.of(context).primaryColor),
                       ),
-                      IconButton(onPressed: onPressed(), icon: Icon(Icons.add, color:Theme.of(context).primaryColor),
-                      ),
+                      SizedBox(width: 20),
 
+                      Expanded(
+                        child:
+                        Column(
+                          children: const [
+                            Text('Margharita Pizza',
+                              style: TextStyle(
+                                fontFamily: 'Monoton-Regular',
+                                fontSize: 18,
+                              ),
+                            ),
+                            Text('4.99\$',
+                              style: TextStyle(
+                                fontFamily: 'Monoton-Regular',
+                                fontSize: 18,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(width: 15),
+                      Row(
+                        children: [
+                          IconButton(onPressed: removeItems,
+                            icon: Icon(Icons.remove, color:Theme.of(context).primaryColor),
+                          ),
+                          IconButton(onPressed: addItems,
+                            icon: Icon(Icons.add, color:Theme.of(context).primaryColor),
+                          ),
+
+                        ],
+                      ),
                     ],
-                  ),
-                ],
-                ),
-              );
-            }),
+                    ),
+                  );
+                }),
             Padding(
               padding: const EdgeInsets.only(top:30.0),
               child: Row(
@@ -95,14 +119,15 @@ class BasketScreen extends StatelessWidget {
                 children: [
                   ElevatedButton(
                     style: ElevatedButton.styleFrom(
-                      fixedSize: Size.fromHeight(60.0),
-                      backgroundColor: Theme.of(context).primaryColor
+                        fixedSize: Size(340, 70),
+                        shape: StadiumBorder(),
+                        backgroundColor: Theme.of(context).primaryColor
                     ),
                     onPressed: ()=>Navigator.pushNamed(context, '/pay_screen'),
                     child: Text('Ödeme ekranına git',
-                    style: TextStyle(
-                      fontSize: 25,
-                    ),
+                      style: TextStyle(
+                        fontSize: 25,
+                      ),
                     ),
                   ),
                 ],
@@ -111,9 +136,7 @@ class BasketScreen extends StatelessWidget {
           ],
         ),
       ),
-      );
-    
+      
+    );
   }
-
-  onPressed() {}
 }
--- lib/screens/basket/pay_screen.dart
+++ lib/screens/basket/pay_screen.dart
@@ -2,41 +2,193 @@
 import 'package:flutter/material.dart';
 import 'package:waitech/config/app_router.dart';
 import 'package:flutter_credit_card/flutter_credit_card.dart';
+import 'package:flutter_credit_card/credit_card_brand.dart';
 
-class PayScreen extends StatelessWidget {
+class PayScreen extends StatefulWidget {
   static const String routeName = '/pay_screen';
+
 
   static Route route() {
     return MaterialPageRoute(
-        builder: (_) => const PayScreen(),
+        builder: (_) => PayScreen(),
         settings: const RouteSettings(name: routeName));
   }
+  @override
+  State<PayScreen> createState() => _PayScreenState();
 
-  const PayScreen({Key? key}) : super(key: key);
 
+}
+
+class _PayScreenState extends State<PayScreen> {
+  String cardNumber = '';
+  String expiryDate = '';
+  String cardHolderName = '';
+  String cvvCode = '';
+  bool isCvvFocused = false;
+  bool useGlassMorphism = false;
+  bool useBackgroundImage = false;
+  OutlineInputBorder? border;
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body:SafeArea(
-        child: Column(
-          /*Center(
-            child: CreditCardWidget(
-              cardNumber: cardNumber,
-              expiryDate: expiryDate,
-              cardHolderName: cardHolderName,
-              cvvCode: cvvCode,
-              showBackView: isCvvFocused, onCreditCardWidgetChange: (CreditCardBrand ) {  }, //true when you want to show cvv(back) view
-            ),
-          ),*/
-        ),
-
-      )
+  void initState() {
+    border = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.grey.withOpacity(0.7),
+        width: 2.0,
+      ),
     );
-
-
+    super.initState();
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Credit Card View Demo',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Theme.of(context).primaryColorDark,
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(title: Text('Ödeme Ekranı',style: TextStyle(color: Colors.black),),
+        backgroundColor: Colors.white),
+        body: Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).primaryColorDark,
+          ),
+          child: SafeArea(
+            child: Column(
+              children: <Widget>[
+                const SizedBox(
+                  height: 30,
+                ),
+                CreditCardWidget(
+                  cardNumber: cardNumber,
+                  expiryDate: expiryDate,
+                  cardHolderName: cardHolderName,
+                  cvvCode: cvvCode,
+                  bankName: '',
+                  showBackView: isCvvFocused,
+                  obscureCardNumber: true,
+                  obscureCardCvv: true,
+                  isHolderNameVisible: true,
+                  cardBgColor: Theme.of(context).primaryColor,
+                  isSwipeGestureEnabled: true,
+                  onCreditCardWidgetChange:
+                      (CreditCardBrand creditCardBrand) {},
+                  customCardTypeIcons: <CustomCardTypeIcon>[
+                    CustomCardTypeIcon(
+                      cardType: CardType.mastercard,
+                      cardImage: Image.asset(
+                        'mastercard.png',
+                        height: 48,
+                        width: 48,
+                      ),
+                    ),
+                  ],
+                ),
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: <Widget>[
+                        CreditCardForm(
+                          formKey: formKey,
+                          obscureCvv: true,
+                          obscureNumber: true,
+                          cardNumber: cardNumber,
+                          cvvCode: cvvCode,
+                          isHolderNameVisible: true,
+                          isCardNumberVisible: true,
+                          isExpiryDateVisible: true,
+                          cardHolderName: cardHolderName,
+                          expiryDate: expiryDate,
+                          themeColor: Colors.blue,
+                          textColor: Colors.black,
+                          cardNumberDecoration: InputDecoration(
+                            labelText: 'Number',
+                            hintText: 'XXXX XXXX XXXX XXXX',
+                            hintStyle: const TextStyle(color: Colors.black),
+                            labelStyle: const TextStyle(color: Colors.black),
+                            focusedBorder: border,
+                            enabledBorder: border,
+                          ),
+                          expiryDateDecoration: InputDecoration(
+                            hintStyle: const TextStyle(color: Colors.black),
+                            labelStyle: const TextStyle(color: Colors.black),
+                            focusedBorder: border,
+                            enabledBorder: border,
+                            labelText: 'Expired Date',
+                            hintText: 'XX/XX',
+                          ),
+                          cvvCodeDecoration: InputDecoration(
+                            hintStyle: const TextStyle(color: Colors.black),
+                            labelStyle: const TextStyle(color: Colors.black),
+                            focusedBorder: border,
+                            enabledBorder: border,
+                            labelText: 'CVV',
+                            hintText: 'XXX',
+                          ),
+                          cardHolderDecoration: InputDecoration(
+                            hintStyle: const TextStyle(color: Colors.black),
+                            labelStyle: const TextStyle(color: Colors.black),
+                            focusedBorder: border,
+                            enabledBorder: border,
+                            labelText: 'Card Holder',
+                          ),
+                          onCreditCardModelChange: onCreditCardModelChange,
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
+                            backgroundColor: Theme.of(context).primaryColor,
+                          ),
+                          child: Container(
+                            margin: const EdgeInsets.all(12),
+                            child: const Text(
+                              'Validate',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontFamily: 'halter',
+                                fontSize: 14,
+                                package: 'flutter_credit_card',
+                              ),
+                            ),
+                          ),
+                          onPressed: () {
+                            if (formKey.currentState!.validate()) {
+                              print('valid!');
+                            } else {
+                              print('invalid!');
+                            }
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
+  void onCreditCardModelChange(CreditCardModel? creditCardModel) {
+    setState(() {
+      cardNumber = creditCardModel!.cardNumber;
+      expiryDate = creditCardModel.expiryDate;
+      cardHolderName = creditCardModel.cardHolderName;
+      cvvCode = creditCardModel.cvvCode;
+      isCvvFocused = creditCardModel.isCvvFocused;
+    });
+  }
 }
